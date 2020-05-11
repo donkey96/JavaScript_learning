75d18ff1c43b720213817de67ff376f3b2071db2
donkey96 <noire96@gmail.com>
2020-05-11 15:55:43 +0900

    簡単な計算

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-document.write('<p>this is "script.js".</p>');
+var price = 12300;
+document.write('<p>価格:' + price + '円</p>');
+const tax = 0.08;
+price = price * (1.0 + tax);
+document.write('<p>税込価格:' + price + '円</p>');
