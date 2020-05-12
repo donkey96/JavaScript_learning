668e9e7ee01a1fc9d1c29d8e569217fdc10e6c0a
donkey96 <noire96@gmail.com>
2020-05-12 17:50:05 +0900

    x月の四季は？？

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-var num = 12345;
-document.write('<p>' + num + 'は、')
-if (num % 2 == 0) {
-  document.write('偶数。</p>')
-} else {
-  document.write('奇数。</p>')
+var month = 4;
+var season;
+
+switch(month) {
+  case 1: season = '冬'; break;
+  case 2: season = '冬'; break;
+  case 3: season = '春'; break;
+  case 4: season = '春'; break;
+  case 5: season = '春'; break;
+  case 6: season = '夏'; break;
+  case 7: season = '夏'; break;
+  case 8: season = '夏'; break;
+  case 9: season = '秋'; break;
+  case 10: season = '秋'; break;
+  case 11: season = '秋'; break;
+  case 12: season = '冬'; break;
+  default: season = '???';
 }
+document.write('<p>' + month + '月は、' + season + 'です。');
