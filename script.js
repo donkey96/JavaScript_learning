7457c17889568795b72b890621f6629ba7e0c021
donkey96 <noire96@gmail.com>
2020-05-12 18:31:00 +0900

    0~xまでの合計を計算する関数

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-const max = 12345; // ★最大の値
-var total = 0
-
-for(var num = 1;num <= max;num++) {
-  total += num;
+function calc(max) {
+  var total = 0;
+  for(var num = 1;num <= max;num++) {
+    total += num;
+  }
+  document.write('<p>' + max + 'までの合計は、' + total + 'です。');
 }
-document.write('<p>' + max + 'までの合計は、' + total + 'です。');
+
+calc(10);
+calc(100);
+calc(1000);
+calc(10000);
