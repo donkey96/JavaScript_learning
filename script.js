cd6291273fe9ca1fcc0fcaa7e1cecb4208e0368d
donkey96 <noire96@gmail.com>
2020-05-14 21:46:25 +0900

    応用

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ var input;
 function initial() {
   msg = document.getElementById('msg');
   input = document.getElementById('input');
-  msg.textContent = '※何か書いて下さい。';
+  msg.textContent = '※本体価格を入力:';
 }
 
 function doclick(e) {
-  msg.textContent = '「' + input.value + '」と書きました。';
+  var price = input.value * 1;
+  var price2 = price * 1.08;
+  msg.textContent = price + '円の税込価格:' + price2 + '円';
 }
