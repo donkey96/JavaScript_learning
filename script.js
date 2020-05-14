0f983490e02e6c66c8e9de85acc961f674fde410
donkey96 <noire96@gmail.com>
2020-05-14 20:54:02 +0900

    コンストラクタ関数

--- script.js
+++ script.js
@@ -1,33 +1,39 @@
-var seiseki = new Object();
-seiseki.data = {'国語':100, '数学':82, '理科':69, '社会':77, '英語':91};
+function Seiseki(name, data) {
+  this.name = name;
+  this.data = data;
 
-// データの数を計算する
-seiseki.count = function() {
-  var n = 0;
-  for(var key in this.data){ n++; }
-  return n;
-}
-// 合計を計算する
-seiseki.total = function() {
-  var total = 0;
-  for(var key in this.data){
-    total += this.data[key];
+  // データの数を計算する
+  this.count = function() {
+    var n = 0;
+    for(var key in this.data){ n++; }
+    return n;
   }
-  return total;
-}
-// 平均を計算する
-seiseki.average = function() {
-  return this.total() / this.count();
-}
-// データを出力する
-seiseki.print = function() {
-  document.write('<ol>');
-  for(var key in this.data) {
-    document.write('<li>' + key + ':' + this.data[key] + '点</li>');
+  // 合計を計算する
+  this.total = function() {
+    var total = 0;
+    for(var key in this.data){
+      total += this.data[key];
+    }
+    return total;
   }
-  document.write('</ol>');
+  // 平均を計算する
+  this.average = function() {
+    return this.total() / this.count();
+  }
+  // データを出力する
+  this.print = function() {
+    document.write('<h2>' + this.name + 'の成績</h2>');
+    document.write('<ol>');
+    for(var key in this.data) {
+      document.write('<li>' + key + ':' + this.data[key] + '点</li>');
+    }
+    document.write('</ol>');
+    document.write('<p>合計:' + this.total() + '<br>');
+    document.write('平均点:' + this.average() + '</p>');
+  }
 }
-// ここからがメインプログラム
-document.write('<h2>定期試験の結果</h2>');
-seiseki.print();
-document.write('<p>合計:' + seiseki.total() + ' 平均点:' + seiseki.average() + ' でした。</p>');
+
+var taro = new Seiseki('たろう', {'国語':65, '数学':79, '理科':88, '社会':59, '英語':61});
+taro.print();
+var sachiko = new Seiseki('さちこ', {'国語':100, '数学':82, '理科':69, '社会':77, '英語':91});
+sachiko.print();
