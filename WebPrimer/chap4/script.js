6c3d74278635dc4e5ff2ecabcfb25ca65895eb68
donkey96 <[noire964@gmail.com]>
2021-03-08 15:46:45 +0900

    円、円弧を描く

--- WebPrimer/chap4/script.js
+++ WebPrimer/chap4/script.js
@@ -2,11 +2,11 @@ function draw() {
   var canvas = document.querySelector("#canvas");
   var context = canvas.getContext("2d");
   context.beginPath();
-  context.fillStyle = "green";
-  context.rect(50, 50, 100, 100);
-  context.fill();
-  context.beginPath();
-  context.fillStyle='yellow';
-  context.rect(100, 100, 100, 100);
+  context.fillStyle='rgba(255, 0, 0, 0.5)';
+  for (let i = 0; i < 10; i++) {
+    context.beginPath();
+    context.arc(100 + 25 * i, 100 + 25 * i, 50, Math.PI / 180 * 0, Math.PI / 180 * 360, false);
+    context.fill();
+   }
   context.fill();
 }
