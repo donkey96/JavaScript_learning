a1a29510919dea1a795cb16cc377b146b9a58ff7
donkey96 <[noire964@gmail.com]>
2021-03-08 14:56:23 +0900

    strokeによる描画

--- WebPrimer/chap4/script.js
+++ WebPrimer/chap4/script.js
@@ -1,8 +1,9 @@
 function draw() {
   var canvas = document.querySelector('#canvas');
   var context = canvas.getContext('2d');
-  context.fillStyle = "rgba(255, 0, 0, 0.5)";
-  context.fillRect(100, 100, 100, 100);
-  context.fillStyle = "rgba(0, 0, 255, 0.5)";
+  context.fillStyle = "rgb(255, 0, 0)";
   context.fillRect(50, 50, 100, 100);
+  context.strokeStyle = "black";
+  context.strokeRect(100, 100, 100, 100);
+  context.strokeRect(50, 50, 100, 100);
 }
