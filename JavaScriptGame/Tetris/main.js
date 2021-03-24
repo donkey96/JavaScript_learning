e0a0988fd7b5a3825e41871dcb52ef3a3a8e553c
donkey96 <[noire964@gmail.com]>
2021-03-24 17:33:28 +0900

    マス目に色をつける

--- JavaScriptGame/Tetris/main.js
+++ JavaScriptGame/Tetris/main.js
@@ -2,6 +2,7 @@
 
 function init() {
   var html = ["<table>"];
+
   for (var y = 0; y < 20; y++) {
     html.push("<tr>");
     for (var x = 0; x < 10; x++) {
@@ -11,4 +12,10 @@ function init() {
   }
   html.push("</table>");
   document.getElementById("view").innerHTML = html.join("");
+
+  var cells = document.getElementsByTagName("td");
+  cells[0].style.backgroundColor = "red";
+  cells[1].style.backgroundColor = "red";
+  cells[2].style.backgroundColor = "red";
+  cells[3].style.backgroundColor = "red";
 }
