875cd15c3c1b9b211e616914c0a88b7498fa4e71
donkey96 <[noire964@gmail.com]>
2021-03-27 15:27:45 +0900

    当たり判定をつける

--- JavaScriptGame/Tetris_canvas/main.js
+++ JavaScriptGame/Tetris_canvas/main.js
@@ -22,59 +22,117 @@ const TETRO_SIZE = 4;
     [ 0, 0, 0, 0 ]
   ];
 
-  // テトロミノの座標
-  let tetro_x = 0;
-  let tetro_y = 0;
+// テトロミノの座標
+let tetro_x = 0;
+let tetro_y = 0;
 
-function init() {
+// フィールドの中身
+let field = [];
+
+// 本プログラム
+function main() {
   let can = document.getElementById("can");
-  let context = can.getContext('2d');
 
   can.width = SCREEN_W;
   can.height = SCREEN_H;
   can.style.border = "4px solid #555"
+  
+  init();
+  drawAll();
 
-  drawTetro();
-
+  // キーボードが押された時の処理
   document.onkeydown = function (e) {
     switch (e.key) {
-      case "ArrowLeft":
-        tetro_x--;
+      case "ArrowLeft": // 左
+        if ( checkMove(-1, 0) ) tetro_x--;
         break;
-      case "ArrowUp":
-        tetro_y--;
+      case "ArrowUp": // 上
+      if ( checkMove(0, -1) ) tetro_y--;
         break;  
-      case "ArrowRight":
-        tetro_x++;
+      case "ArrowRight": // 右
+      if ( checkMove(+1, 0) ) tetro_x++;
         break;
-      case "ArrowDown":
-        tetro_y++;
+      case "ArrowDown": // 下
+      if ( checkMove(0, +1) ) tetro_y++;
         break;
-      case " ":
+      case " ": // スペース
         break;
     }
-    drawTetro();
+    drawAll();
   }
 }
-  // テトロミノを表示する関数
-  function drawTetro() {
-    let can = document.getElementById("can");
-    let context = can.getContext('2d');
 
-    context.clearRect(0, 0, SCREEN_W, SCREEN_H);
-    
+// 初期化
+function init() {
+  for (let y = 0; y < FIELD_ROW; y++ ) {
+    field[y] = [];
+    for (let x = 0; x < FIELD_COL; x++) {
+      field[y][x] = 0;
+    }
+  }
 
-    for (let y = 0; y < TETRO_SIZE; y++ ) {
-      for (let x = 0; x < TETRO_SIZE; x++) {
-        if ( tetro[y][x] == 1 ) {
-          let px = ( tetro_x + x ) * BLOCK_SIZE;
-          let py = ( tetro_y + y ) * BLOCK_SIZE;
+  // テスト
+  field[5][8] = 1;
+  field[19][0] = 1;
+  field[19][9] = 1;
+}
 
-          context.fillStyle = 'red';
-          context.fillRect(px, py, BLOCK_SIZE, BLOCK_SIZE);  
-          context.strokeStyle='black';
-          context.strokeRect(px, py, BLOCK_SIZE, BLOCK_SIZE);
+// 全てを描画する関数
+function drawAll() {
+  let can = document.getElementById("can");
+  let context = can.getContext('2d');
+
+  context.clearRect(0, 0, SCREEN_W, SCREEN_H);
+  
+  // フィールドを作成
+  for (let y = 0; y < FIELD_ROW; y++ ) {
+    for (let x = 0; x < FIELD_COL; x++) {
+      if ( field[y][x] ) {
+        drawBlock(x, y);
+      }
+    }
+  }
+
+  // テトロミノを作成
+  for (let y = 0; y < TETRO_SIZE; y++ ) {
+    for (let x = 0; x < TETRO_SIZE; x++) {
+      if ( tetro[y][x] ) {
+        drawBlock(tetro_x + x, tetro_y + y);
+      }
+    }
+  }
+}
+
+// ブロック１つを描画する関数
+function drawBlock(x, y) {
+  let can = document.getElementById("can");
+  let context = can.getContext('2d');    
+
+  let px = x * BLOCK_SIZE;
+  let py = y * BLOCK_SIZE;
+
+  context.fillStyle = 'red';
+  context.fillRect(px, py, BLOCK_SIZE, BLOCK_SIZE);  
+  context.strokeStyle='black';
+  context.strokeRect(px, py, BLOCK_SIZE, BLOCK_SIZE);
+
+}
+
+function checkMove(mx, my) {
+  for (let y = 0; y < TETRO_SIZE; y++ ) {
+    for (let x = 0; x < TETRO_SIZE; x++) {
+      let nx = tetro_x + mx + x;
+      let ny = tetro_y + my + y;
+      if (tetro[y][x]) {
+        if ( ny < 0 ||
+             nx < 0 ||
+             ny >= FIELD_ROW ||
+             nx >= FIELD_COL ||
+             field[ny][nx] ) {
+               return false;
         }
       }
     }
   }
+  return true;
+}
